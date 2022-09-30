d0621c7eac2cbc361ed2d5d01ec7a88916858176
Liana Sabino Alves <liana.sabino@matriz.fornecedoramaquinas.com.br>
2022-09-30 14:01:52 -0300

    first commit

---
+++ src/pages/Filme/index.js
@@ -0,0 +1,89 @@
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate} from 'react-router-dom';
+import './filme-info.css';
+
+import api from '../../services/api';
+
+function Filme(){
+    const { id } = useParams();
+    const navigate = useNavigate()
+
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() =>{
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params:{
+                    api_key: "9a8aff367d5e460a11fbbcc0aadd6f97",
+                    language: "pt-BR",
+                }
+            })
+            .then((response)=> {
+                setFilme(response.data);
+                setLoading(false);
+
+            })
+            .catch(()=>{
+                navigate("/", { replace: true})
+                return;
+
+            })
+
+        }
+
+        loadFilme();
+
+        return () => {
+
+        }
+    }, [navigate, id])
+
+    function salvarFilme(){
+        const minhaLista = localStorage.getItem("@primeflix");
+
+        let filmesSalvos = JSON.parse(minhaLista) || [];
+
+        const hasFilme = filmesSalvos.some( (filmesSalvos)  => filmesSalvos.id === filme.id)
+
+        if(hasFilme) {
+            alert("ESSE FILME JÁ ESTÁ NA LISTA");
+            return;
+        }
+
+        filmesSalvos.push(filme);
+        localStorage.setItem("@primeflix",JSON.stringify(filmesSalvos));
+        alert("FILME SALVO COM SUCESSO!")
+    }
+
+    if(loading) {
+        return(
+            <div className="filme-info">
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
+
+    return(
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+            <div className="area-buttons">
+                <button onClick={salvarFilme}>Salvar</button>
+                <button>
+                    <a target="blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
+                        Trailer
+                    </a>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Filme;
